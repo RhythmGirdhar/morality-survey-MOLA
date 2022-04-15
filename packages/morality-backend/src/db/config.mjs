3015052ebafab8e5ef4d9bfcef3b9a83f575165e
Rhythm Girdhar <ridhigirdhar3@gmail.com>
2022-04-15 15:51:26 -0700

    Update config.mjs

--- packages/morality-backend/src/db/config.mjs
+++ packages/morality-backend/src/db/config.mjs
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 export const dbConfig = {
   mongoose: mongoose,
-  url: "mongodb+srv://rohan:12345@cluster0.onnea.mongodb.net/morality-survey",
+  url: "mongodb+srv://{username}:{password}@cluster0.onnea.mongodb.net/morality-survey",
 };
